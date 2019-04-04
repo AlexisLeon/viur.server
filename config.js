30d9ec60a835bb0b13eee596cbf25b0d88307893
Alexis Leon <alexis.leon@icloud.com>
2019-04-04 14:18:24 -0600

    Load config from env vars

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    nexmo: {
+        brandName: process.env.brandName,
+        apiKey: process.env.apiKey,
+        apiSecret: process.env.apiSecret,
+    },
+    firestore: {
+        database: process.env.database,
+    }
+}
